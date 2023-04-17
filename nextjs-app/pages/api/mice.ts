697f429bf71fc41d66d99c4c56aff4338fb443d2
kashifulhaque <haque.kashiful7@gmail.com>
2023-04-17 18:09:36 +0530

    Added api endpoint

---
+++ nextjs-app/pages/api/mice.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const password = process.env.PASSWORD;
+const uri = `mongodb+srv://mongodb:${password}@cluster-ap-south-1.rzpcloa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+});
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  let result;
+  try {
+    await client.connect();
+    console.log('connected');
+    const collection = await client.db('mouse_db').collection('mouse_collection');
+    result = await collection.find({}).toArray();
+  } catch(e) {
+    console.log(e);
+  }
+  finally {
+    await client.close();
+  }
+
+  res.status(200).json(result);
+}
